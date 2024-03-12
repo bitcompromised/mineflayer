1c072059ae70b152db3c58afc405a3ca80b0057f
bitcompromised <135661730+bitcompromised@users.noreply.github.com>
2024-03-12 01:49:41 -0700

    Create HttpProxy.js

---
+++ Proxy/HttpProxy.js
@@ -0,0 +1,67 @@
+import Http from 'http';
+import mc from 'minecraft-protocol'
+import { ProxyAgent } from 'proxy-agent';
+
+
+
+const mcServer = {
+    host: "", // Replace with your Minecraft server IP
+    port: 25565,
+};
+
+const proxy = {
+    host: "",
+    port: ,
+    username: '',
+    password: ''
+};
+
+const proxyHost = proxy.host;
+const proxyPort = proxy.port;
+
+const client = mc.createClient({
+  version: '1.8',
+  connect: (client) => {
+    const req = Http.request({
+      host: proxyHost,
+      port: proxyPort,
+      method: 'CONNECT',
+      path: `${mcServer.host}:${mcServer.port}`,
+    })
+    req.end()
+
+    req.on('connect', (res, stream) => {
+      client.setSocket(stream)
+      client.emit('connect')
+    })
+  },
+  host: mcServer.host,
+  port: mcServer.port,
+  agent: new ProxyAgent(`http://${proxy.username}:${proxy.password}@${proxy.host}`),
+  username: "proxy",
+  auth: 'microsoft',
+  
+})
+
+client.on('kicked', (reason)=>{
+	console.log(reason);
+})
+client.on('connect', function () {
+  console.info('connected')
+})
+client.on('disconnect', function (packet) {
+  console.log('disconnected: ' + packet.reason)
+})
+client.on('end', function (reason) {
+  console.log('Connection lost', reason)
+})
+client.on('playerChat', function (message){
+	console.log(message);
+})
+client.on('packet', function (packet){
+  //console.log(packet);
+})
+client.on('spawn',()=>{
+	//console.log(stream);
+	console.log('Bot spawned? with name ', client.username);
+})
